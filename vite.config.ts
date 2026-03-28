15fbdc52d1115206c893087938dc8ee52dd4c9a7
renatosilva2020 <professorrsilva2020@gmail.com>
2026-03-27 21:18:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({mode}) => {
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['favicon.ico', 'https://i.ibb.co/nNBBZZhd/e-ESCOLAR.png', 'mask-icon.svg'],
+        includeAssets: ['favicon.ico', 'https://i.ibb.co/MyN2VfTv/logo-eemp.png', 'mask-icon.svg'],
         manifest: {
           name: 'e-Escolar',
           short_name: 'e-Escolar',
@@ -26,12 +26,12 @@ export default defineConfig(({mode}) => {
           start_url: './',
           icons: [
             {
-              src: 'https://i.ibb.co/nNBBZZhd/e-ESCOLAR.png',
+              src: 'https://i.ibb.co/MyN2VfTv/logo-eemp.png',
               sizes: '192x192',
               type: 'image/png'
             },
             {
-              src: 'https://i.ibb.co/nNBBZZhd/e-ESCOLAR.png',
+              src: 'https://i.ibb.co/MyN2VfTv/logo-eemp.png',
               sizes: '512x512',
               type: 'image/png'
             }
