fb8d4e920b1a4f5a7197c58cee6a586764a12d51
renatosilva2020 <professorrsilva2020@gmail.com>
2026-03-28 07:18:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,8 @@ export default defineConfig(({mode}) => {
         registerType: 'autoUpdate',
         includeAssets: ['favicon.ico', 'https://i.ibb.co/wZY6D3g2/logo-e.png', 'mask-icon.svg'],
         manifest: {
-          name: 'e-Escolar MÁRIO PORTO',
-          short_name: 'e-Escolar MÁRIO PORTO',
+          name: 'e-Escolar MárioPorto',
+          short_name: 'e-Escolar MárioPorto',
           description: 'Sistema de autenticação para acesso ao portal do aluno',
           theme_color: '#ffffff',
           background_color: '#ffffff',
